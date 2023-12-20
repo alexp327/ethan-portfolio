1cdfc18210cb650d34e24a181e340f68497d7161
Alex Perry <alex.perry422@gmail.com>
2023-12-20 17:12:05 -0500

    layout edits

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,13 @@ import data from '@/db.json';
 import Skills from '@/components/skills';
 import PersonalHighlights from '@/components/personal-highlights';
 import { Mail } from 'lucide-react';
+import Footer from '@/components/footer';
 
 export default function Home() {
   return (
-    <main className='mx-auto max-w-7xl'>
+    <main>
       {/* About Me */}
-      <div className='relative flex min-h-[80vh] w-full border-2 border-red-300 px-2'>
+      <div className='relative mx-auto flex min-h-[80vh] w-full max-w-7xl px-2'>
         <div className='flex w-full flex-col justify-center'>
           <h1 className='text-6xl'>
             Hello, I&apos;m <br />{' '}
@@ -43,65 +44,70 @@ export default function Home() {
         </div> */}
       </div>
       {/* Projects */}
-      <div className='space-y-4 border-2 border-blue-300 p-4'>
-        <h2 className='text-center text-3xl font-bold'>Projects</h2>
-        <ul className='mx-auto grid max-w-2xl grid-cols-2 gap-4'>
-          {data.projects.map((project) => {
-            return (
-              <li
-                key={project.title}
-                className='flex flex-col gap-1 rounded-md bg-secondary p-4 transition-opacity hover:cursor-pointer hover:opacity-70'
-              >
-                <AspectRatio ratio={1 / 1} className='z-0'>
-                  <Image
-                    src={project.image}
-                    alt='project example'
-                    className='mx-auto h-full rounded-md object-cover'
-                    width={500}
-                    height={500}
-                  />
-                </AspectRatio>
-                <h3 className='text-xl font-bold'>{project.title}</h3>
-                <p>{project.description}</p>
-                <ul className='flex flex-wrap gap-3'>
-                  {project.tools.map((tool) => {
-                    return (
-                      <li
-                        key={tool}
-                        className='rounded-md bg-primary/60 px-3 py-1 text-sm'
-                      >
-                        {tool}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </li>
-            );
-          })}
-        </ul>
+      <div className='bg-secondary'>
+        <div className='mx-auto max-w-7xl space-y-4 p-4'>
+          <h2 className='text-center text-3xl font-bold'>Projects</h2>
+          <ul className='mx-auto grid max-w-2xl grid-cols-2 gap-4'>
+            {data.projects.map((project) => {
+              return (
+                <li
+                  key={project.title}
+                  className='flex flex-col gap-1 rounded-md bg-background p-4 transition-opacity hover:cursor-pointer hover:opacity-70'
+                >
+                  <AspectRatio ratio={1 / 1} className='z-0'>
+                    <Image
+                      src={project.image}
+                      alt='project example'
+                      className='mx-auto h-full rounded-md object-cover'
+                      width={500}
+                      height={500}
+                    />
+                  </AspectRatio>
+                  <h3 className='text-xl font-bold'>{project.title}</h3>
+                  <p>{project.description}</p>
+                  <ul className='flex flex-wrap gap-3'>
+                    {project.tools.map((tool) => {
+                      return (
+                        <li
+                          key={tool}
+                          className='rounded-md bg-primary/60 px-3 py-1 text-sm'
+                        >
+                          {tool}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
       {/* Skills */}
-      <div className='border-2 border-green-300 p-4'>
+      <div className='p-4'>
         <h2 className='text-center text-3xl font-bold'>Skills</h2>
         <Skills />
         <PersonalHighlights />
       </div>
       {/* Resume */}
-      <div className='space-y-4 border-2 border-orange-300 p-4'>
-        <h2 className='text-center text-3xl font-bold'>Resume</h2>
-        <iframe
-          src='/Alex_Perry_Resume.pdf'
-          //@ts-expect-error
-          frameborder='0'
-          className='h-screen w-full'
-        ></iframe>
+      <div className='bg-secondary'>
+        <div className='mx-auto max-w-7xl space-y-4 p-4'>
+          <h2 className='text-center text-3xl font-bold'>Resume</h2>
+          <iframe
+            src='/Alex_Perry_Resume.pdf'
+            //@ts-expect-error
+            frameborder='0'
+            className='h-screen w-full'
+          ></iframe>
+        </div>
       </div>
-      <div className='space-y-8 border-2 border-green-300 p-4'>
+      <div className='mx-auto max-w-7xl space-y-8 p-4'>
         <h2 className='text-center text-3xl font-bold'>Contact Me</h2>
         <h4 className='mx-auto flex w-fit gap-6 text-xl'>
           <Mail size={32} /> eperry2658@gmail.com
         </h4>
       </div>
+      <Footer></Footer>
     </main>
   );
 }
---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+  return <div className='flex flex-col items-center bg-primary'>Footer</div>;
+};
+
+export default Footer;
